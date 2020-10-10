3a7472cdf15a9726817fa29aeea2d7bb2f07cedb
tanvir <tanvirgeek@gmail.com>
2020-10-10 09:47:57 +0600

    Initial Commit

---
+++ BargraphDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BargraphDemo
+//
+//  Created by MD Tanvir Alam on 10/10/20.
+//  Copyright © 2020 MD Tanvir Alam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
